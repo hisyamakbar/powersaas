40adc7492c911c96dc666733f6eacdc67971f6e2
Hisyam <akbar.hisyam1@gmail.com>
2024-06-11 12:02:21 +0700

    chore: Update tailwind.config.js and output.css
    This commit updates the tailwind.config.js file to include new styles for the container and fontFamily. It also updates the output.css file with new styles for the container, media queries, and various utility classes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			container: {
+				center: true,
+				padding: "100px",
+			},
+			fontFamily: {
+				poppins: ["Poppins", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
 };
